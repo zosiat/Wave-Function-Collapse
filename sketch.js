e41954c10f2c39a7b2bcbe2fbf1b1e031e22fec4
Zosia Trela <ztrela@ucsc.edu>
2024-10-23 10:21:22 -0700

    more tiles

--- sketch.js
+++ sketch.js
@@ -29,12 +29,20 @@ function removeDuplicatedTiles(tiles) {
 function setup() {
   createCanvas(400, 400);
   //randomSeed(15);
+  // tiles[0] = new Tile(tileImages[0], ['AAA', 'AAA', 'AAA', 'AAA']);
+  // tiles[1] = new Tile(tileImages[1], ['ABA', 'ABA', 'ABA', 'AAA']);
+  // tiles[2] = new Tile(tileImages[2], ['BAA', 'AAB', 'AAA', 'AAA']);
+  // tiles[3] = new Tile(tileImages[3], ['BAA', 'AAA', 'AAB', 'AAA']);
+  // tiles[4] = new Tile(tileImages[4], ['ABA', 'ABA', 'AAA', 'AAA']);
+  // tiles[5] = new Tile(tileImages[5], ['ABA', 'AAA', 'ABA', 'AAA']);
+  // tiles[6] = new Tile(tileImages[6], ['ABA', 'ABA', 'ABA', 'ABA']);
 
+  //4 values are for the different sides [top, right, bottom, left] (i think)
    tiles[0] = new Tile(tileImages[0], ['AAA', 'AAA', 'AAA', 'AAA']);
    tiles[1] = new Tile(tileImages[1], ['ABA', 'ABA', 'ABA', 'AAA']);
-   tiles[2] = new Tile(tileImages[2], ['BAA', 'AAB', 'AAA', 'AAA']);
-   tiles[3] = new Tile(tileImages[3], ['BAA', 'AAA', 'AAB', 'AAA']);
-   tiles[4] = new Tile(tileImages[4], ['ABA', 'ABA', 'AAA', 'AAA']);
+   tiles[2] = new Tile(tileImages[2], ['ABA', 'ABA', 'AAA', 'AAA']);
+   tiles[3] = new Tile(tileImages[3], ['AAA', 'AAA', 'ABA', 'AAA']);
+   tiles[4] = new Tile(tileImages[4], ['AAA', 'AAA', 'AAA', 'AAA']);
    tiles[5] = new Tile(tileImages[5], ['ABA', 'AAA', 'ABA', 'AAA']);
    tiles[6] = new Tile(tileImages[6], ['ABA', 'ABA', 'ABA', 'ABA']);
 
